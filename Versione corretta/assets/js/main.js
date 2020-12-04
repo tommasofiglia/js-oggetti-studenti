b053dcbdeb4f02c57ddaff1fd7870677266fcf1e
Tommaso Figlia <tommasofiglia@gmail.com>
2020-12-04 15:57:16 +0100

    Caricata versione corretta con stampa in console log

---
+++ Versione corretta/assets/js/main.js
@@ -0,0 +1,63 @@
+/********************* CONSEGNA *************************
+Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
+Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+*********************************************************/
+
+
+$(function () {
+
+//Oggetto che descrive uno studente
+var studente = {
+  nome: "Tommaso",
+  cognome: "Figlia",
+  età: 25
+}
+
+console.log(studente);
+
+//Array di oggetti di studenti
+var studenti = [
+  {
+    nome: "Tommaso",
+    cognome: "Figlia",
+    età: 25
+  },
+  {
+    nome: "Pinco",
+    cognome: "Pallo",
+    età: 32
+  },
+  {
+    nome: "Francesco",
+    cognome: "Pannofino",
+    età: 56
+  },
+  {
+    nome: "Giuseppe",
+    cognome: "Conte",
+    età: 53
+  }
+]
+
+studenti.forEach((item) => {
+  console.log(item.nome + " " + item.cognome);
+});
+
+//Aggiunta di uno studente indicato dall'utente attraverso i prompt
+
+studenti.push(
+  {
+    nome: prompt("Inserisci il nome di un nuovo studente"),
+    cognome: prompt("Inserisci il cognome del nuovo studente"),
+    eta: Number(prompt("Inserisci l'età del nuovo studente"))
+  }
+)
+
+console.log(studenti);
+
+
+
+
+
+});
